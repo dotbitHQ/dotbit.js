7b2c711d25dfe33d99d0c5a29c3ac629d346e6cb
jeffjing <zgayjjf@qq.com>
2022-07-11 18:17:43 +0800

    feat: improve example

--- example/browser/index.js
+++ example/browser/index.js
@@ -1,6 +1,7 @@
 import './polyfill'
-import { createInstance, EthersSigner } from '../../lib.esm/index.js'
-import { ethers } from 'ethers'
+import { createInstance } from '../../lib.esm/index.js'
+import { ProviderSigner } from '../../lib.esm/signers/ProviderSigner'
+import { typedDataFromMetamask } from './typedData'
 
 const $ = document.querySelector.bind(document)
 const $inputAccount = $('#input-account')
@@ -8,15 +9,15 @@ const $inputSubAccount = $('#input-sub-account')
 const $buttonRecords = $('#btn-records')
 const $buttonMint = $('#btn-mint')
 const $buttonInfo = $('#btn-into')
+const $buttonPersonalSign = $('#btn-personal-sign')
+const $buttonSignTypedData = $('#btn-sign-typed-data')
 const $codeOutput = $('#code-output')
 
 async function main () {
-  const provider = new ethers.providers.Web3Provider(window.ethereum)
-  const ethersSigner = provider.getSigner()
-  const signer = new EthersSigner(ethersSigner)
+  const signer = new ProviderSigner(window.ethereum)
   const dotbit = createInstance({ network: 'testnet', signer })
 
-  const addresses = await provider.send('eth_requestAccounts')
+  const addresses = await window.ethereum.send('eth_requestAccounts')
 
   const address = addresses[0]
 
@@ -61,6 +62,18 @@ async function main () {
     catch (e) {
       window.alert(e.message)
     }
+  })
+
+  $buttonPersonalSign.addEventListener('click', async (a) => {
+    signer.signData('0xtest').then(res => {
+      $codeOutput.innerHTML = res
+    })
+  })
+
+  $buttonSignTypedData.addEventListener('click', async (a) => {
+    signer.signData(typedDataFromMetamask, true).then(res => {
+      $codeOutput.innerHTML = res
+    })
   })
 }
 
--- example/node/index.js
+++ example/node/index.js
@@ -1,5 +1,34 @@
 const { createInstance } = require('../../lib/index')
+const { ethers, Wallet } = require('ethers')
+const { EthersSigner } = require('../../lib/index')
 
-const dotbit = createInstance()
+const address = '0x7df93d9F500fD5A9537FEE086322a988D4fDCC38'
+const privateKey1 = '87d8a2bccdfc9984295748fa2058136c8131335f59930933e9d4b3e74d4fca42'
+const provider = new ethers.providers.InfuraProvider('goerli')
+const wallet = new Wallet(privateKey1, provider)
+const signer = new EthersSigner(wallet)
 
-dotbit.records('imac.bit').then(console.log)
+const dotbit = createInstance({
+  network: 'testnet',
+  signer,
+})
+
+async function main () {
+  await dotbit.records('imac.bit').then(console.log)
+  await dotbit.accountInfo('imac.bit').then(console.log)
+
+  const account = dotbit.account('imac.bit')
+
+  const res = await account.mintSubAccount({
+    account: '001.imac.bit',
+    keyInfo: {
+      coin_type: '60',
+      key: await wallet.getAddress(),
+    },
+    registerYears: 1,
+  })
+
+  console.log(res)
+}
+
+void main()
