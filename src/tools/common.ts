22ca3dee7e8d998df6e3049343712e2ea43565b5
jeffjing <zgayjjf@qq.com>
2022-09-06 18:18:33 +0800

    feat: add `isEmptyAddress` function

--- src/tools/common.ts
+++ src/tools/common.ts
@@ -6,6 +6,14 @@ export function remove0x (str: string): string {
   return str.startsWith('0x') ? str.substring(2) : str
 }
 
+/**
+ * Check if a given address is empty
+ * @param address
+ */
+export function isEmptyAddress (address: string): boolean {
+  return !address || address === '0x' || address === '0x0' || address === '0x0000000000000000000000000000000000000000'
+}
+
 export function sleep (ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
--- tests/tools/common.test.ts
+++ tests/tools/common.test.ts
@@ -1,4 +1,4 @@
-import { pad0x } from '../../src/tools/common'
+import { isEmptyAddress, pad0x } from '../../src/tools/common'
 
 describe('pad0x', function () {
   it('starts with 0x', function () {
@@ -15,5 +15,37 @@ describe('pad0x', function () {
     const padded = pad0x(str)
 
     expect(padded).toBe(`0x${str}`)
+  })
+})
+
+describe('isEmptyAddress', function () {
+  it('empty', function () {
+    const isEmpty = isEmptyAddress('')
+
+    expect(isEmpty).toBe(true)
+  })
+
+  it('0x', function () {
+    const isEmpty = isEmptyAddress('0x')
+
+    expect(isEmpty).toBe(true)
+  })
+
+  it('0x0', function () {
+    const isEmpty = isEmptyAddress('0x0')
+
+    expect(isEmpty).toBe(true)
+  })
+
+  it('0x0000000000000000000000000000000000000000', function () {
+    const isEmpty = isEmptyAddress('0x0000000000000000000000000000000000000000')
+
+    expect(isEmpty).toBe(true)
+  })
+
+  it('0x123', function () {
+    const isEmpty = isEmptyAddress('0x123')
+
+    expect(isEmpty).toBe(false)
   })
 })
