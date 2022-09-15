a6e53136e02a86862f61d957d1cb8fedc2284982
jeffjing <zgayjjf@qq.com>
2022-09-15 20:24:06 +0800

    feat: add a plugin template

---
+++ packages/plugin-template/src/index.ts
@@ -0,0 +1,28 @@
+import { DotBit, BitAccount, BitPluginBase, sleep } from 'dotbit'
+
+export class BitPluginTemplate implements BitPluginBase {
+  version = '0.0.1'
+  name = 'BitPluginTemplate'
+
+  onInstall (dotbit: DotBit) {
+    console.log('This function will be invoked when plugin installed, you can add some methods to DotBit like the code below:')
+
+    dotbit.flyAccount = function (this: DotBit, account: string) {
+      const bitAccount = this.account(account)
+      return bitAccount.fly().then(word => word + ' to sky')
+    }
+  }
+
+  onUninstall (dotbit: DotBit) {
+    console.log('This function will be invoked when plugin uninstalled')
+  }
+
+  onInitAccount (bitAccount: BitAccount) {
+    console.log('This function will be invoked when .bit account initialized')
+
+    bitAccount.fly = async function (this: BitAccount): Promise<string> {
+      await sleep(100)
+      return `${this.account} is flying`
+    }
+  }
+}
---
+++ packages/plugin-template/tests/index.test.ts
@@ -0,0 +1,20 @@
+import { DotBit } from 'dotbit'
+import { BitPluginTemplate } from '../src/index'
+
+const dotbit = new DotBit()
+const pluginTemplate = new BitPluginTemplate()
+
+dotbit.installPlugin(pluginTemplate)
+
+describe('flyAccount', function () {
+  it('should work', async function () {
+    expect(await dotbit.flyAccount('imac.bit')).toBe('imac.bit is flying to sky')
+  })
+})
+
+describe('fly', function () {
+  it('should work', async function () {
+    const account = dotbit.account('imac.bit')
+    expect(await account.fly()).toBe('imac.bit is flying')
+  })
+})
