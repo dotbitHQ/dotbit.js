b7ce81e41c45b7cc7b8f4e12c0ca1afa16deb24b
yj <1095778225@qq.com>
2022-11-19 22:40:52 +0800

    feat: add Plugin-web3mq

---
+++ packages/plugin-web3mq/src/types.ts
@@ -0,0 +1,23 @@
+import { DotBit, BitAccount } from 'dotbit';
+import { Client, PageParams } from 'web3-mq';
+
+declare module 'dotbit' {
+  export interface DotBit {
+    web3Mq: Client | null;
+    sendMessageToUser: (message: string, userId: string) => void;
+    onMessage: (callback: any) => void;
+    searchWeb3mqUser: (did: string, didType: DID_TYPE_ENUM) => Promise<any[]>;
+    getMessageList: (page: PageParams, userId: string) => Promise<void>;
+  }
+  export interface BitAccount {
+    web3Mq: any;
+    funcWeb3MQ: () => void;
+  }
+}
+
+export enum DID_TYPE_ENUM {
+  ETH = 'ETH',
+  WEB3_MQ = 'web3-mq',
+  DOTBIT = 'dotbit',
+  ENS = 'ens',
+}
---
+++ packages/plugin-web3mq/src/utils.ts
@@ -0,0 +1,64 @@
+import { Client } from "web3-mq";
+import { DotBit } from "dotbit";
+import { ethers } from "ethers";
+import { DID_TYPE_ENUM } from "./types";
+
+const provider = new ethers.providers.AnkrProvider();
+
+export const init = async (appKey: string) => {
+  const fastUrl = await Client.init({
+    connectUrl: localStorage.getItem("FAST_URL"),
+    app_key: appKey,
+  });
+  localStorage.setItem("FAST_URL", fastUrl);
+};
+
+export const hasKeys = () => {
+  const PrivateKey = localStorage.getItem("PRIVATE_KEY") || "";
+  const PublicKey = localStorage.getItem("PUBLICKEY") || "";
+  const userid = localStorage.getItem("USERID") || "";
+  if (PrivateKey && PublicKey && userid) {
+    return { PrivateKey, PublicKey, userid };
+  }
+  return null;
+};
+
+export const signMetaMask = async () => {
+  const { PrivateKey, PublicKey, userid } = await Client.register.signMetaMask({
+    signContentURI: "https://www.web3mq.com",
+  });
+  localStorage.setItem("PRIVATE_KEY", PrivateKey);
+  localStorage.setItem("PUBLICKEY", PublicKey);
+  localStorage.setItem("USERID", userid);
+  return { PrivateKey, PublicKey, userid };
+};
+
+export const getUserIdByAddress = async (client: Client, address: string) => {
+  const searchUsersResponse = await client.user.searchUsers(address);
+  console.log(searchUsersResponse, "searchUsersResponse");
+  //@ts-ignore
+  if (searchUsersResponse && searchUsersResponse.result) {
+    //@ts-ignore
+    return searchUsersResponse.result;
+  }
+  return "";
+};
+
+export const getAddressByDid = async (
+  dotbit: DotBit,
+  did: string,
+  didType: DID_TYPE_ENUM
+) => {
+  if (didType === DID_TYPE_ENUM.DOTBIT) {
+    const data = await dotbit.records(did);
+    let ethAddress = data && data.find((item) => item.key === "address.eth");
+    return ethAddress?.value || "";
+  }
+
+  if (didType === DID_TYPE_ENUM.ENS) {
+    const address = await provider.resolveName(did);
+    console.log(address, "address");
+    return address;
+  }
+  return "";
+};
