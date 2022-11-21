b26df5a517e9cfba335728c3e7097c9d2b5d79f7
jeffjing <zgayjjf@qq.com>
2022-11-22 01:53:31 +0800

    chore: add lint commands

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: 'blockabc/typescript',
+  // We should ignore these files, otherwise it will conflict with tsconfig.json and throw error when run `eslint` manually
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/docs/linting/TROUBLESHOOTING.md#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file
+  ignorePatterns: [
+    'lib',
+    'lib.esm',
+  ],
 }
